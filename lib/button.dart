32a23fe8508ba2be2416d540e612bdaa263965d8
Samantha Lau <samantha.lhe@gmail.com>
2023-08-20 20:20:24 +0800

    Create button widget

---
+++ lib/button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+  final Color color;
+  final Color backgroundColor;
+
+  const Button(
+      {required this.text,
+      required this.onClicked,
+      this.color = Colors.white,
+      this.backgroundColor = Colors.brown,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+          backgroundColor: backgroundColor,
+        ),
+        onPressed: onClicked,
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 20,
+            color: color,
+          ),
+        ));
+  }
+}
